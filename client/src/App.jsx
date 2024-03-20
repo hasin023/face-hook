d8f14f82c7be28d69a8a39b73a21a31a230abe76
Hasin <hasinmahtab.alvee@gmail.com>
2024-03-20 20:15:27 +0600

    Addded hook-form login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import NotFoundPage from './pages/NotFoundPage';
---
+++ client/src/components/auth/LoginForm.jsx
@@ -0,0 +1,52 @@
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import Field from "../common/Field";
+
+const LoginForm = () => {
+
+    const navigate = useNavigate();
+    const { register, handleSubmit, formState: { errors }, } = useForm();
+
+    const submitForm = (formData) => {
+        console.log(formData);
+        navigate("/");
+    };
+
+    return (
+        <form className="border-b border-[#3F3F3F] pb-10 lg:pb-[60px]" onSubmit={handleSubmit(submitForm)}>
+
+            <Field label="Email" error={errors.email}>
+                <input {...register("email", { required: "Email is required" })}
+                    className={`auth-input ${errors.email ? "border-red-500" : "border-gray-200"}`}
+                    name="email"
+                    type="email"
+                    id="email"
+                />
+            </Field>
+
+            <Field label="Password" error={errors.password}>
+                <input {...register("password", {
+                    required: "Password is required",
+                    minLength: { value: 6, message: "Password must be at least 6 characters" }
+                })}
+                    className={`auth-input ${errors.password ? "border-red-500" : "border-gray-200"}`}
+                    name="password"
+                    type="password"
+                    id="password"
+                />
+            </Field>
+
+            <Field>
+                <button
+                    className="auth-input bg-lwsGreen font-bold text-deepDark transition-all hover:opacity-90"
+                    type="submit"
+                >
+                    Login
+                </button>
+            </Field>
+
+        </form>
+    )
+}
+
+export default LoginForm;
